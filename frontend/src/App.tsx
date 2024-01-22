bced66b9d976a00e549efe28c4ad44f67cf15779
k-isaka <isk.rugby@gmail.com>
2024-01-22 23:07:43 +0900

    fix to deploy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import leagueData2022 from './datas/2022_league_one_game_data.json';
 import leagueData2023 from './datas/2023_league_one_game_data.json';
@@ -23,6 +22,25 @@ function App() {
           <th>ビジターチーム選手リスト</th>
         </tr>
         {leagueData2022.map((data) => (
+          <tr>
+            <td>{data.id}</td>
+            <td>{data.basic_info}</td>
+            <td>{data.home_team}</td>
+            <td>{data.home_team_score}</td>
+            <td>{data.away_team_score}</td>
+            <td>{data.away_team}</td>
+            <td>{data.date}</td>
+            <td>{data.weather}</td>
+            <td>{data.spectator}</td>
+            <td>
+              <PlayerObjectList playerList={data.home_team_player_list} />
+            </td>
+            <td>
+              <PlayerObjectList playerList={data.away_team_player_list} />
+            </td>
+          </tr>
+        ))}
+        {leagueData2023.map((data) => (
           <tr>
             <td>{data.id}</td>
             <td>{data.basic_info}</td>
