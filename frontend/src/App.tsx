7cdfe0e0bb73626c666027034622b96e863f3854
k-isaka <isk.rugby@gmail.com>
2024-01-24 23:27:49 +0900

    feat: 年度を選択して表を表示する際にエラーが発生しないように修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,15 +7,27 @@ import { Button } from './stories/Button';
 import { Game } from './types/game';
 
 function App() {
-  const [year, setYear] = useState(2023);
   const dataOf2022 = leagueData2022 as Game[];
   const dataOf2023 = leagueData2023 as Game[];
+  const [data, setData] = useState(dataOf2022);
+  const [year, setYear] = useState(2022);
+
+  const handleYear = (year: number) => {
+    setYear(year);
+    if (year === 2022) {
+      setData(dataOf2022);
+    }
+    if (year === 2023) {
+      setData(dataOf2023);
+    }
+  };
+
   return (
     <div className='App'>
-      <Button primary={true} label={'2022'} onClick={() => setYear(2022)} />
-      <Button primary={true} label={'2023'} onClick={() => setYear(2023)} />
+      <Button primary={true} label={'2022'} onClick={() => handleYear(2022)} />
+      <Button primary={true} label={'2023'} onClick={() => handleYear(2023)} />
       <h1>{year}シーズン</h1>
-      <GameInfoTable gameList={eval(`dataOf${year}`)} />
+      <GameInfoTable gameList={data} />
     </div>
   );
 }
