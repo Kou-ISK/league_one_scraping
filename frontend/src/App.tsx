7cd153c5671692c2bf276bf03f8392e73ea7255e
k-isaka <isk.rugby@gmail.com>
2024-01-25 23:59:52 +0900

    feat: 表を描画する際にDataGridを使用するように修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import leagueData2023 from './datas/2023_league_one_game_data.json';
 import { GameInfoTable } from './stories/gameInfoTable';
 import { Button } from './stories/Button';
 import { Game } from './types/game';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { PlayerObjectList } from './stories/playerObjectList';
 
 function App() {
   const dataOf2022 = leagueData2022 as Game[];
@@ -21,13 +23,40 @@ function App() {
       setData(dataOf2023);
     }
   };
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'basic_info', headerName: 'basic_info', width: 200 },
+    { field: 'home_team', headerName: 'home_team', width: 200 },
+    { field: 'home_team_score', headerName: 'home_team_score', width: 70 },
+    { field: 'away_team_score', headerName: 'away_team_score', width: 70 },
+    { field: 'away_team', headerName: 'away_team', width: 200 },
+    { field: 'date', headerName: 'date', width: 100 },
+    { field: 'weather', headerName: 'weather', width: 70 },
+    { field: 'spectator', headerName: 'spectator', width: 70 },
+    {
+      field: 'home_team_player_list',
+      headerName: 'home_team_player_list',
+      width: 120,
+      valueGetter: (params: GridValueGetterParams) => {
+        return <PlayerObjectList playerList={params} />;
+      },
+    },
+    {
+      field: 'away_team_player_list',
+      headerName: 'away_team_player_list',
+      width: 120,
+      valueGetter: (params: GridValueGetterParams) => {
+        return <PlayerObjectList playerList={params} />;
+      },
+    },
+  ];
 
   return (
     <div className='App'>
       <Button primary={true} label={'2022'} onClick={() => handleYear(2022)} />
       <Button primary={true} label={'2023'} onClick={() => handleYear(2023)} />
       <h1>{year}シーズン</h1>
-      <GameInfoTable gameList={data} />
+      <DataGrid rows={data} columns={columns} checkboxSelection />
     </div>
   );
 }
