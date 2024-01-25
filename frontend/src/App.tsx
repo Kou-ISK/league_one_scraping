ac33b07d96e37296d5294597fa7695d13d93f2dd
k-isaka <isk.rugby@gmail.com>
2024-01-26 00:21:43 +0900

    feat: 表を描画する際にDataGridを使用するように修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,58 +5,40 @@ import leagueData2023 from './datas/2023_league_one_game_data.json';
 import { GameInfoTable } from './stories/gameInfoTable';
 import { Button } from './stories/Button';
 import { Game } from './types/game';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import { PlayerObjectList } from './stories/playerObjectList';
 
 function App() {
   const dataOf2022 = leagueData2022 as Game[];
   const dataOf2023 = leagueData2023 as Game[];
   const [data, setData] = useState(dataOf2022);
   const [year, setYear] = useState(2022);
+  const [isButton2022Selected, setIsButton2022Selected] = useState(true);
 
   const handleYear = (year: number) => {
     setYear(year);
     if (year === 2022) {
       setData(dataOf2022);
+      setIsButton2022Selected(true);
     }
     if (year === 2023) {
       setData(dataOf2023);
+      setIsButton2022Selected(false);
     }
   };
-  const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'basic_info', headerName: 'basic_info', width: 200 },
-    { field: 'home_team', headerName: 'home_team', width: 200 },
-    { field: 'home_team_score', headerName: 'home_team_score', width: 70 },
-    { field: 'away_team_score', headerName: 'away_team_score', width: 70 },
-    { field: 'away_team', headerName: 'away_team', width: 200 },
-    { field: 'date', headerName: 'date', width: 100 },
-    { field: 'weather', headerName: 'weather', width: 70 },
-    { field: 'spectator', headerName: 'spectator', width: 70 },
-    {
-      field: 'home_team_player_list',
-      headerName: 'home_team_player_list',
-      width: 120,
-      valueGetter: (params: GridValueGetterParams) => {
-        return <PlayerObjectList playerList={params} />;
-      },
-    },
-    {
-      field: 'away_team_player_list',
-      headerName: 'away_team_player_list',
-      width: 120,
-      valueGetter: (params: GridValueGetterParams) => {
-        return <PlayerObjectList playerList={params} />;
-      },
-    },
-  ];
 
   return (
     <div className='App'>
-      <Button primary={true} label={'2022'} onClick={() => handleYear(2022)} />
-      <Button primary={true} label={'2023'} onClick={() => handleYear(2023)} />
+      <Button
+        primary={!isButton2022Selected}
+        label={'2022'}
+        onClick={() => handleYear(2022)}
+      />
+      <Button
+        primary={isButton2022Selected}
+        label={'2023'}
+        onClick={() => handleYear(2023)}
+      />
       <h1>{year}シーズン</h1>
-      <DataGrid rows={data} columns={columns} checkboxSelection />
+      <GameInfoTable gameList={data} />
     </div>
   );
 }
--- frontend/src/stories/gameInfoTable.tsx
+++ frontend/src/stories/gameInfoTable.tsx
@@ -1,59 +1,23 @@
 import { Game } from '../types/game';
-import { PlayerObjectList } from './playerObjectList';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 interface GameInfoTableProps {
   gameList: Game[];
 }
 export const GameInfoTable = (props: GameInfoTableProps) => {
   const gameList = props.gameList;
-  return (
-    <TableContainer sx={{ marginX: '1vw', justifySelf: 'center' }}>
-      <Table stickyHeader>
-        <TableHead>
-          <TableRow>
-            <TableCell sx={{ width: '10px' }}>Game ID</TableCell>
-            <TableCell>基本情報</TableCell>
-            <TableCell>ホストチーム</TableCell>
-            <TableCell sx={{ width: '10px' }}>ホストチーム得点</TableCell>
-            <TableCell sx={{ width: '10px' }}>ビジターチーム得点</TableCell>
-            <TableCell>ビジターチーム</TableCell>
-            <TableCell sx={{ width: '10px' }}>試合日</TableCell>
-            <TableCell sx={{ width: '10px' }}>天候</TableCell>
-            <TableCell sx={{ width: '10px' }}>観客動員数</TableCell>
-            <TableCell>ホストチーム選手リスト</TableCell>
-            <TableCell>ビジターチーム選手リスト</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {gameList.map((data) => (
-            <TableRow>
-              <TableCell>{data.id}</TableCell>
-              <TableCell>{data.basic_info}</TableCell>
-              <TableCell>{data.home_team}</TableCell>
-              <TableCell>{data.home_team_score}</TableCell>
-              <TableCell>{data.away_team_score}</TableCell>
-              <TableCell>{data.away_team}</TableCell>
-              <TableCell>{data.date}</TableCell>
-              <TableCell>{data.weather}</TableCell>
-              <TableCell>{data.spectator}</TableCell>
-              <TableCell>
-                <PlayerObjectList playerList={data.home_team_player_list} />
-              </TableCell>
-              <TableCell>
-                <PlayerObjectList playerList={data.away_team_player_list} />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'basic_info', headerName: '基本情報', width: 600 },
+    { field: 'home_team', headerName: 'ホストチーム', width: 300 },
+    { field: 'home_team_score', headerName: 'ホストチーム得点', width: 150 },
+    { field: 'away_team_score', headerName: 'ビジターチーム得点', width: 150 },
+    { field: 'away_team', headerName: 'ビジターチーム', width: 300 },
+    { field: 'date', headerName: '日時', width: 200 },
+    { field: 'weather', headerName: '天候', width: 200 },
+    { field: 'spectator', headerName: '観客動員数', width: 200 },
+    { field: 'referee_name', headerName: 'レフェリー', width: 200 },
+    { field: 'stadium', headerName: 'スタジアム', width: 300 },
+  ];
+  return <DataGrid rows={gameList} columns={columns} checkboxSelection />;
 };
