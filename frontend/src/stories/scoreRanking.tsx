676290f02605c18e5fa72f5678e6fe3738ddd8bd
k-isaka <isk.rugby@gmail.com>
2024-02-13 18:39:08 +0900

    feat: styleの調整

--- frontend/src/stories/scoreRanking.tsx
+++ frontend/src/stories/scoreRanking.tsx
@@ -1,17 +1,19 @@
+import { Card } from '@mui/material';
+
 interface ScoreRankingProps {
   rankingTop10: any[];
 }
 
 export const ScoreRanking = (props: ScoreRankingProps) => {
   return (
-    <ul>
+    <>
       {props.rankingTop10.map((scoreInfo: any, index: number) => {
         return (
-          <li>
+          <Card sx={{ margin: '10px', padding: '10px' }}>
             {index + 1}. {scoreInfo.scorer}: {scoreInfo.point}
-          </li>
+          </Card>
         );
       })}
-    </ul>
+    </>
   );
 };
