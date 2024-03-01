8483198242ae68192fcf67f181fe798ebba7de19
k-isaka <isk.rugby@gmail.com>
2024-03-01 20:53:27 +0900

    style: 試合詳細画面のアコーディオンを削除

--- frontend/src/stories/playerObject.tsx
+++ frontend/src/stories/playerObject.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { Player } from '../types/player';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { TEAM_MASTER_DATA, LEAGUE_ONE_ROOT_URL } from '../variables';
+import { Paper } from '@mui/material';
 
 interface PlayerObjectProps {
   player: Player;
@@ -13,29 +10,21 @@ interface PlayerObjectProps {
 export const PlayerObject = (props: PlayerObjectProps) => {
   const player = props.player;
   return (
-    <Accordion sx={{ marginTop: '3px' }}>
-      <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
-        <Typography sx={{ fontSize: '15px' }}>
-          {player.number}.{' '}
-          <a
-            href={
-              LEAGUE_ONE_ROOT_URL +
-              'player/' +
-              TEAM_MASTER_DATA.flatMap((team) => team.player_list).find(
-                (player) => player.name
-              )?.player_id
-            }
-          >
-            {player.name}
-          </a>
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Typography>Position: {player.position}</Typography>
-        <Typography>Height: {player.height}</Typography>
-        <Typography>Weight: {player.weight}</Typography>
-        <Typography>Age: {player.age}</Typography>
-      </AccordionDetails>
-    </Accordion>
+    <Paper sx={{ margin: '5px' }}>
+      <Typography sx={{ padding: '10px' }}>
+        {player.number}.{' '}
+        <a
+          href={
+            LEAGUE_ONE_ROOT_URL +
+            'player/' +
+            TEAM_MASTER_DATA.flatMap((team) => team.player_list).find(
+              (player) => player.name
+            )?.player_id
+          }
+        >
+          {player.name}
+        </a>
+      </Typography>
+    </Paper>
   );
 };
